81c636b262fdf9341fce1927370e60832cbc9e0c
Gabriel Han <gabrielhan104@gmail.com>
2021-06-02 12:34:45 -0700

    stupid

--- helper.js
+++ helper.js
@@ -2,7 +2,7 @@ document.getElementById("youtubejunk").onclick = contactContent("youtube")
 document.getElementById("instagramjunk").onclick = contactContent("instagram")
 
 function contactContent(message) {
-    console.log("contacting content.js")
+    console.log("contacting content.js for "+message)
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {greeting: message}, function(response) {
             console.log(response);
