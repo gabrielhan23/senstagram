e8ff3ce712e33fd6be6c34263d556f3ef9d2eb29
Timothy Oh <timothyohoc@gmail.com>
2021-06-02 23:16:57 -0700

    fixed button thing

--- helper.js
+++ helper.js
@@ -1,8 +1,8 @@
-document.getElementById("youtubejunk").onclick = contactContent("youtube")
-document.getElementById("instagramjunk").onclick = contactContent("instagram")
 
-function contactContent(message) {
-    console.log("contacting content.js for "+message)
+let buttonThing = document.getElementById("instagramjunk")
+buttonThing.onclick = function(){
+    let message = "instagram"
+    console.log("contacting content.js for " + message)
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {greeting: message}, function(response) {
             console.log(response);
